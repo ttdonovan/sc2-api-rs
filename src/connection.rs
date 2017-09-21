99f2a35cf3c099e4b747435f3dc1ca3a1386ec00
Tanner Donovan <ttdonovan@gmail.com>
2017-09-20 22:10:02 -0700

    first attempt at setting up thread for Client and send_ch

--- src/connection.rs
+++ src/connection.rs
@@ -4,18 +4,23 @@ use sc2api::{response, Response};
 use prost::Message;
 use ws::{self, connect, Handler, Handshake, Result as WsResult};
 
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::mpsc::{channel, Receiver, Sender};
 use std::thread::{self, JoinHandle};
 
 #[derive(Debug)]
 pub struct Connection {
     recv_ch: Receiver<response::Response>,
+    send_ch: Sender<()>,
+    running: Arc<AtomicBool>,
     conn_thread: JoinHandle<()>,
 }
 
 pub struct Client {
     tx: Sender<response::Response>,
-    out: ws::Sender,
+    // rx: Receiver<()>,
+    // out: ws::Sender,
 }
 
 impl Handler for Client {
@@ -37,14 +42,44 @@ impl Handler for Client {
 impl Connection {
     pub fn connect() -> Result<Connection> {
         let (recv_tx, recv_rx) = channel();
+        let (send_tx, send_rx): (Sender<()>, Receiver<()>) = channel();
+        let running = Arc::new(AtomicBool::new(true));
 
         let thread = thread::Builder::new()
             .name("StarCraft connection".into())
             .spawn(move || {
-                connect("ws://127.0.0.1:5000/sc2api", |out| {
+                connect("ws://127.0.0.1:5000/sc2api", |_out| {
+// // FIXME: compilation error[E0507]
+// // error[E0507]: cannot move out of captured outer variable in an `FnMut` closure
+// //   --> src/connection.rs:55:32
+// //    |
+// // 45 |         let (send_tx, send_rx): (Sender<()>, Receiver<()>) = channel();
+// //    |                       ------- captured outer variable
+// // ...
+// // 63 |                         .spawn(move || {
+// //    |                                ^^^^^^^ cannot move out of captured outer variable in an `FnMut` closure
+//                     let _t = thread::Builder::new()
+//                         .name("Client sender".into())
+//                         .spawn(move || {
+//                             // TODO: replace "loop" with "while running <AtomicBool(true)>"
+//                             loop {
+//                                 use std::time::Duration;
+//
+//                                 println!("client sender listening...");
+//                                 match send_rx.recv() {
+//                                     Ok(_) => println!("got a message"),
+//                                     Err(_) => panic!(),
+//                                 }
+//
+//                                 thread::sleep(Duration::from_millis(500));
+//                             }
+//                         })
+//                         .unwrap();
+
                     Client {
                         tx: recv_tx.clone(),
-                        out: out,
+                        // rx: send_rx,
+                        // out: out,
                     }
                 }).unwrap()
             })
@@ -52,6 +87,8 @@ impl Connection {
 
         let connection = Connection {
             recv_ch: recv_rx,
+            send_ch: send_tx,
+            running: running,
             conn_thread: thread,
         };
 
@@ -65,5 +102,17 @@ impl Connection {
             Ok(r) => Ok(r),
             Err(_) => panic!(),
         }
+    }
+
+    // TODO: accept a request and send to the channel (send_ch)
+    pub fn send_request(&self) {
+        self.send_ch.send(()).expect("failed to send...");
+    }
+}
+
+impl Drop for Connection {
+    fn drop(&mut self) {
+        self.running.store(false, Ordering::Relaxed);
+        // TODO: do we need to wait for listening thread with send_ch (rx) to close for a graceful shutdown?
     }
 }
